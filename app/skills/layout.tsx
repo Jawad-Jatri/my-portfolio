491c1624cceb327afd3da6d2b57551e79b931900
jawad <jawad.jatri@gmail.com>
2024-10-30 18:51:48 +0600

    other layouts initialized

---
+++ app/skills/layout.tsx
@@ -0,0 +1,11 @@
+export default function SkillsLayout({
+                                         children,
+                                     }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
